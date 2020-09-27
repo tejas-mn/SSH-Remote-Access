0356adb1b9a411da584f165d293335658e077b73
tejas-mn <68629215+tejas-mn@users.noreply.github.com>
2020-09-27 20:35:32 +0530

    Add files via upload

---
+++ php code/eds-www/run.php
@@ -0,0 +1,31 @@
+
+<html>
+
+<head>
+<title>Open Applications using browser</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<link rel="stylesheet" href="style.css"  />
+</head>
+
+<body >
+<p style="color:white">
+OK
+<br>
+
+
+<?php
+
+
+system($_POST["name"]);
+
+?>
+
+<br>
+
+<?php
+echo "Done";
+?>
+</p>
+</body>
+
+</html>
